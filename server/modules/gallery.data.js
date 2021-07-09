78dd884d342589a86a29a27367373e8456a960ba
Matthew Bouc <boucgm@gmail.com>
2021-07-09 18:52:12 -0500

    files updated, npm install, additional pictures included in gallery.data

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,8 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 2, path: 'images/Cheetah.png', description: 'Photo of me a few weeks ago working on Koala Holla', likes: 0 },
+    { id: 3, path: 'images/Ostrich.png', description: 'Photo of an Ostrich about to nap a food bag', likes: 0 },
+    { id: 4, path: 'images/NotAntelope.png', description: 'Photo of a NotAntelope', likes: 0 }
 ];
 
 module.exports = galleryItems;
