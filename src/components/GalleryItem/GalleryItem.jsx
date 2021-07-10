46a1fc9b5c5b035b569622f836caf2d41f56682c
Matthew Bouc <boucgm@gmail.com>
2021-07-09 19:34:18 -0500

    toggle of picture and text functional

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,10 +1,22 @@
+import React, {useState} from 'react';
+
 function GalleryItem({item}){
+
+    const [isPicture, setIsPicture] = useState(true);
+
+    const togglePicture = () => {
+        console.log('Clicked Picture to Toggle');
+        setIsPicture(!isPicture);
+    }
+
     return (
         <>
             <div key={item.id}>
-                <img height="200px" src={item.path} />
-                <p>{item.description}</p>
-                <h4>Likes: {item.likes}</h4>
+                <form onClick={togglePicture}>
+                    { isPicture && <img height="200px" src={item.path} /> }
+                    { !isPicture && <p>{item.description}</p> }
+                </form>
+                    <h4>Likes: {item.likes}</h4>
             </div>
         </>
     )
