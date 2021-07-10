46efba394d7fee2c71bec2d9ba6bf4ad3e0490be
Matthew Bouc <boucgm@gmail.com>
2021-07-09 22:03:36 -0500

    Database GET connection set up and working properly

--- Database.sql
+++ Database.sql
@@ -1,3 +1,6 @@
+-- database name:  react_gallery
+--DROP TABLE galleryItems;
+
 CREATE TABLE galleryItems (
 	id SERIAL PRIMARY KEY,
 	path VARCHAR(100) NOT NULL,
@@ -7,6 +10,3 @@ CREATE TABLE galleryItems (
 
 INSERT INTO galleryItems (path, description)
 	VALUES ('images/goat_small.jpg', 'Photo of a goat taken at Glacier National Park.'), ('images/Cheetah.png', 'Photo of me a few weeks ago working on Koala Holla'), ('images/Ostrich.png', 'Photo of an Ostrich about to nap a food bag'), ('images/NotAntelope.png', 'Photo of a NotAntelope');
-
-
---DROP TABLE galleryItems;
--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,24 +1,39 @@
+const { query } = require('express');
 const express = require('express');
 const router = express.Router();
-const galleryItems = require('../modules/gallery.data');
+const pool = require('../modules/pool.js');
+//BASE MODE // const galleryItems = require('../modules/gallery.data');
 
 // DO NOT MODIFY THIS FILE FOR BASE MODE
 
-// PUT Route
-router.put('/like/:id', (req, res) => {
-    console.log(req.params);
-    const galleryId = req.params.id;
-    for(const galleryItem of galleryItems) {
-        if(galleryItem.id == galleryId) {
-            galleryItem.likes += 1;
-        }
-    }
-    res.sendStatus(200);
-}); // END PUT Route
+//BASE MODE// PUT Route
+// router.put('/like/:id', (req, res) => {
+//     console.log(req.params);
+//     const galleryId = req.params.id;
+//     for(const galleryItem of galleryItems) {
+//         if(galleryItem.id == galleryId) {
+//             galleryItem.likes += 1;
+//         }
+//     }
+//     res.sendStatus(200);
+//}); // END PUT Route
 
-// GET Route
+// BASE MODE // GET Route
+// router.get('/', (req, res) => {
+//     res.send(galleryItems);
+//}); // END GET Route
+
 router.get('/', (req, res) => {
-    res.send(galleryItems);
-}); // END GET Route
+    const queryText = `SELECT * FROM galleryItems ORDER BY id ASC;`;
+    pool.query(queryText)
+    .then(result => {
+        console.log('Database sending result: ', result);
+        res.send(result.rows);
+    })
+    .catch(error => {
+        console.log('Database GET failed', error);
+        res.sendStatus(500);
+    });
+});
 
 module.exports = router;
