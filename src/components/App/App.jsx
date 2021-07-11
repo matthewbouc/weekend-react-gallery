1b750a990dab66e31e5c73ca8554913b22325a7c
Matthew Bouc <boucgm@gmail.com>
2021-07-10 21:02:20 -0500

    Testing material UI

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,12 +2,79 @@ import React from 'react';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 import './App.css';
-
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import CameraIcon from '@material-ui/icons/PhotoCamera';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
 import GalleryList from '../GalleryList/GalleryList';
+
+
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  albumLayout: {
+    backgroundColor: theme.palette.info.light,
+    color: theme.palette.common.black,
+  },
+  icon: {
+    marginRight: theme.spacing(2),
+    color: theme.palette.common.black,
+  },
+  heroContent: {
+    backgroundColor: theme.palette.grey[800],
+    padding: theme.spacing(8, 0, 6),
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  cardGrid: {
+    backgroundColor: theme.palette.grey[900],
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardMedia: {
+    paddingTop: '56.25%', // 16:9
+  },
+  cardContent: {
+    backgroundColor: theme.palette.grey[800],
+    flexGrow: 1,
+  },
+  footer: {
+    backgroundColor: theme.palette.grey[800],
+    padding: theme.spacing(6),
+  },
+}));
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 
 function App() {
 
+
+  const classes = useStyles();
     /**
      * Run GET request on page load.
      */
@@ -49,13 +116,72 @@ function App() {
     }
     
     return (
-      <div className="App">
+      <>
+      {/* <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
         <p>Gallery goes here</p>
         <GalleryList putLikeButton={putLikeButton} galleryItems={galleryItems} />
-      </div>
+      </div> */}
+
+
+        <CssBaseline />
+            <AppBar className={classes.albumLayout} position="relative">
+              <Toolbar>
+                <CameraIcon className={classes.icon} />
+                <Typography variant="h6" color="inherit" noWrap>
+                  Album layout
+                </Typography>
+              </Toolbar>
+            </AppBar>
+            <main>
+              {/* Hero unit */}
+              <div className={classes.heroContent}>
+                <Container maxWidth="sm">
+                  <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+                    Album layout
+                  </Typography>
+                  <Typography variant="h5" align="center" color="textSecondary" paragraph>
+                    Something short and leading about the collection below—its contents, the creator, etc.
+                    Make it short and sweet, but not too short so folks don&apos;t simply skip over it
+                    entirely.
+                  </Typography>
+                  <div className={classes.heroButtons}>
+                    <Grid container spacing={2} justifyContent="center">
+                      <Grid item>
+                        <Button variant="contained" color="primary">
+                          Main call to action
+                        </Button>
+                      </Grid>
+                      <Grid item>
+                        <Button variant="outlined" color="primary">
+                          Secondary action
+                        </Button>
+                      </Grid>
+                    </Grid>
+                  </div>
+                </Container>
+              </div>
+              <Container className={classes.cardGrid} maxWidth="md">
+                {/* End hero unit */}
+                <GalleryList galleryItems={galleryItems} putLikeButton={putLikeButton}/>
+              </Container>
+            </main>
+      {/* Footer */}
+      <footer className={classes.footer}>
+        <Typography variant="h6" align="center" gutterBottom>
+          Footer
+        </Typography>
+        <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+          Something here to give the footer a purpose!
+        </Typography>
+        <Copyright />
+      </footer>
+      </>
+
+
+
     );
 }
 
--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,8 +1,15 @@
 import React, {useState} from 'react';
+import FavoriteRoundedIcon from '@material-ui/icons/FavoriteRounded';
+import FavoriteBorderRoundedIcon from '@material-ui/icons/FavoriteBorderRounded';
+
+import './GalleryItem.css';
+import { classExpression } from '@babel/types';
+
 
 function GalleryItem({item, putLikeButton}){
     //isPicture is used to toggle picture/text active.
     const [isPicture, setIsPicture] = useState(true);
+    const [isHeartFull, setIsHeartFull] = useState(false);
     
     /**
      * Changes the state of isPicture when <form> is clicked.
@@ -12,9 +19,11 @@ function GalleryItem({item, putLikeButton}){
         setIsPicture(!isPicture);
     }
 
+
     /**
      * Accepts item.id and item.likes when Like button is clicked.  Adds +1 to likes.
      * Sends a PUT request to update the database with new Likes amount.
+     * This could be done on the server queryText likes: likes+1
      * @param {Number} itemId 
      * @param {Number} currentLikes 
      */
@@ -23,14 +32,26 @@ function GalleryItem({item, putLikeButton}){
         putLikeButton(itemId, currentLikes)
     }
 
+    const toggleHeart = () => {
+        setIsHeartFull(!isHeartFull);
+    }
+
     return (
         <>
             <div key={item.id}>
-                <form onClick={togglePicture}>
-                    { isPicture && <img height="200px" src={item.path} /> }
-                    { !isPicture && <p>{item.description}</p> }
-                </form>
-                <button onClick={()=> handleLikeButton(item.id, item.likes)} >Like This Picture</button>
+                <div onClick={togglePicture}>
+                    { isPicture && <img className="cardImage"  src={item.path} /> }
+                    { !isPicture && <p className="cardText">{item.description}</p> }
+                </div>
+                <div onMouseEnter={()=> toggleHeart()}
+                     onMouseLeave={() => toggleHeart()}>
+                { !isHeartFull && <FavoriteBorderRoundedIcon 
+                    onClick={()=> handleLikeButton(item.id, item.likes)}
+                    color="secondary" fontSize="medium" />}
+                { isHeartFull && <FavoriteRoundedIcon 
+                    onClick={()=> handleLikeButton(item.id, item.likes)}
+                    color="secondary" fontSize="medium" />}
+                </div>
                 <h4>Likes: {item.likes}</h4>
             </div>
         </>
